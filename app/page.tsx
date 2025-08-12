779abbd962302a25885ab96961431052dcbe799e
JaeChang <wockd9600@naver.com>
2025-08-12 20:41:51 +0900

    2

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,35 @@
 
 export default function Home() {
   const address = "대전 동구 광명길 64";
-  const handleNaverMapClick = () => {
+  const handleNaverMapClick1 = () => {
     if (!address) return;
-    // window.open(`https://map.naver.com/p/search/${encodeURIComponent(address)}`, '_blank');
-    // const url = `https://map.naver.com/p/directions/,,/${encodeURIComponent(address)},,/-/car`;
+    window.open(`https://map.naver.com/p/search/${encodeURIComponent(address)}`, '_blank');
+  };
+  const handleNaverMapClick2 = () => {
+    if (!address) return;
+    const url = `https://map.naver.com/p/directions/,,/${encodeURIComponent(address)},,/-/car`;
+    window.open(url, '_blank');
+  };
+  const handleNaverMapClick3 = () => {
+    if (!address) return;
     const url = `https://map.naver.com/p/directions/-/${encodeURIComponent(address)}/-/car`;
     window.open(url, '_blank');
   };
 
-  const handleKakaoMapClick = () => {
+  const handleKakaoMapClick1 = () => {
     if (!address) return;
-    // window.open(`https://map.kakao.com/link/search/${encodeURIComponent(address)}`, '_blank');
+    window.open(`https://map.kakao.com/link/search/${encodeURIComponent(address)}`, '_blank');
+    // const url = `https://map.kakao.com/link/route?ep=${encodeURIComponent(address)}`;
+    // window.open(url, '_blank');
+  };
+  const handleKakaoMapClick2 = () => {
+    if (!address) return;
     const url = `https://map.kakao.com/link/route?ep=${encodeURIComponent(address)}`;
+    window.open(url, '_blank');
+  };
+  const handleKakaoMapClick3 = () => {
+    if (!address) return;
+    const url = `https://map.kakao.com/?map_type=TYPE_MAP&target=car&rt2=${encodeURIComponent(address)}`;
     window.open(url, '_blank');
   };
 
@@ -28,18 +45,46 @@ export default function Home() {
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <div className="flex justify-center space-x-2 mt-4">
         <button
-          onClick={handleNaverMapClick}
+          onClick={handleNaverMapClick1}
           className="bg-[#03C75A] text-white px-4 py-2 rounded-md text-sm font-medium flex items-center justify-center"
           style={{ minWidth: '100px' }}
         >
-          네이버 지도
+          네이버 지도1
         </button>
         <button
-          onClick={handleKakaoMapClick}
+          onClick={handleNaverMapClick2}
+          className="bg-[#03C75A] text-white px-4 py-2 rounded-md text-sm font-medium flex items-center justify-center"
+          style={{ minWidth: '100px' }}
+        >
+          네이버 지도2
+        </button>
+        <button
+          onClick={handleNaverMapClick3}
+          className="bg-[#03C75A] text-white px-4 py-2 rounded-md text-sm font-medium flex items-center justify-center"
+          style={{ minWidth: '100px' }}
+        >
+          네이버 지도3
+        </button>
+        <button
+          onClick={handleKakaoMapClick1}
           className="bg-[#FEE500] text-black px-4 py-2 rounded-md text-sm font-medium flex items-center justify-center"
           style={{ minWidth: '100px' }}
         >
-          카카오 지도
+          카카오 지도1
+        </button>
+        <button
+          onClick={handleKakaoMapClick2}
+          className="bg-[#FEE500] text-black px-4 py-2 rounded-md text-sm font-medium flex items-center justify-center"
+          style={{ minWidth: '100px' }}
+        >
+          카카오 지도2
+        </button>
+        <button
+          onClick={handleKakaoMapClick3}
+          className="bg-[#FEE500] text-black px-4 py-2 rounded-md text-sm font-medium flex items-center justify-center"
+          style={{ minWidth: '100px' }}
+        >
+          카카오 지도3
         </button>
         <button
           onClick={handleTmapClick}
